54935df1e2586c9fd538d8adccb56e1d1335f6ae
Junaid Nawaz <shaikhmohammedjunaidnawaz@gmail.com>
2026-02-19 16:37:05 +0530

    test push

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 
-// Slack slash commands are sent as application/x-www-form-urlencoded
+// Slack slash commands are sent as application/x-www-form-urlencoded test
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
